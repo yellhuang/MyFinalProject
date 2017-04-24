9b7a67044f749b0094778b5dadbd1e1f535a6b39
yellhuang <yellhuang63@gmail.com>
2017-04-24 17:21:10 +0800

    2017/4/24

---
+++ src/stringcut/example/Test2.java
@@ -0,0 +1,26 @@
+package stringcut.example;
+
+/**
+ * Created by Student on 2017/4/24.
+ */
+public class Test2 {
+    public static void main(String args[]){
+        String str1 = "asxd";
+        String str2 = "eyhj";
+        String str3 = "qqqqq";
+
+        String[] arrStr = {str1, str2, str3};
+        String[] arrKey = {"as", "q"};
+
+        for(String s1: arrStr){
+            for(String s2: arrKey){
+                if(s1.contains(s2)){
+                    System.out.println(s1 + "包含" + s2);
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
