1bde2a9a9600d838069d78ceef512f7e698143e7
yellhuang <yellhuang63@gmail.com>
2017-04-28 00:04:51 +0800

    把從資料庫讀出的資料放到ArrayList再印出

--- src/connect/sqlserver/example/TestSelect.java
+++ src/connect/sqlserver/example/TestSelect.java
@@ -1,6 +1,8 @@
 package connect.sqlserver.example;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by yellow on 2017/4/23.
@@ -8,17 +10,24 @@ import java.sql.*;
 public class TestSelect {
     public static void main(String[] args) {
 
+        List<String> fanclubList = new ArrayList<>();
+
         // 建立連線
         try (
-                Connection connection = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=DMDB", "sa", "yellow");
+                Connection connection = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=testdb", "sa", "yellow");
                 Statement statement = connection.createStatement();
-                ResultSet rs = statement.executeQuery("select * from [Weather-Norminal-C]")
+                ResultSet rs = statement.executeQuery("select id from information")
         ) {
             // 產生Statement物件：執行SQL指令
             // 產生ResultSet物件
             // 	一筆一筆讀資料
             while (rs.next()) {
-                System.out.println("ID.=" + rs.getInt("ID"));
+//                System.out.println("ID.=" + rs.getString("ID"));
+                fanclubList.add(rs.getString("ID"));
+            }
+
+            for(String str:fanclubList){
+                System.out.println("ID.=" + str);
             }
 
         } catch (SQLException e) {
